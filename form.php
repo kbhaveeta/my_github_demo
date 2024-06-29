7bbaad80cbc7d9a00b459db6e5beb99bc8118886
kbhaveeta <157223292+kbhaveeta@users.noreply.github.com>
2024-06-29 16:23:53 +0500

    upload php file

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="table2.php" method="POST">
+        
+    <p>Number 1</p>
+    <input type="text" name="user" placeholder="Type your name">
+    <p>EMAIL</p>
+    <input type="email" name="gmail" placeholder="Type your email">
+    <br><br>
+    <input type="submit" value="submit" name="done">
+    </form>
+</body>
+</html>
